cd1fa3e332ec6b8359ebbb607fc1bb297027f479
DevLoop <alex.detu01@gmail.com>
2025-11-12 00:24:37 +0100

    cors

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'X-Client-Fingerprint'],
   exposedHeaders: ['Set-Cookie']
 }));
 
